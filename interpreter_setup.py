d9a6fad6bab4900319c448b05b2d74228bf12fff
Zafeer Hafeez <zafeerhafeez1738@gmail.com>
2023-08-26 08:48:31 +0500

    modified:   interpreter_setup.py

--- interpreter_setup.py
+++ interpreter_setup.py
@@ -1,33 +1,45 @@
 # write code for selinium to open google.com
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.options import Options
 
-driver = webdriver.Edge("./webdriver/msedgedriver.exe")
+
+options = Options()
+print(dir(options))  
+options.capabilities["args"] = ["--disable-user-media-security", "--use-fake-ui-for-media-stream"]
+# options.use_chromium = True
+# # options.add_argument("headless")
+# options.add_argument("disable-gpu")
+# # options.add_argument("--disable-user-media-security")
+# options.add_argument("use-fake-ui-for-media-stream")
+
+
+driver = webdriver.Edge("./webdriver/msedgedriver.exe", )
 driver.get("https://my.openphone.com/login")
 
-# find by css selector
-email_input = driver.find_element_by_css_selector("input[placeholder='Email address...']")
+# # find by css selector
+# email_input = driver.find_element_by_css_selector("input[placeholder='Email address...']")
 
-# type azamkhanfiverr@gmail.com
-email_input.send_keys("azamkhanfiverr@gmail.com")
+# # type azamkhanfiverr@gmail.com
+# email_input.send_keys("azamkhanfiverr@gmail.com")
 
-# 2nd button of the page
-continue_button = driver.find_elements_by_css_selector("button")[1]
+# # 2nd button of the page
+# continue_button = driver.find_elements_by_css_selector("button")[1]
 
-# click
-continue_button.click()
+# # click
+# continue_button.click()
 
-code = driver.find_element_by_css_selector("input[name='code']")
-code.send_keys("123456")
-code.send_keys(Keys.ENTER)
+# code = driver.find_element_by_css_selector("input[name='code']")
+# code.send_keys("123456")
+# code.send_keys(Keys.ENTER)
 
-alert = None
+# alert = None
 
-while alert is None:
-    try:
-        alert = driver.find_element_by_css_selector("div[role='alert']")
-        if alert is not None:
-            print(alert.text)
-            break
-    except:
-        pass
+# while alert is None:
+#     try:
+#         alert = driver.find_element_by_css_selector("div[role='alert']")
+#         if alert is not None:
+#             print(alert.text)
+#             break
+#     except:
+#         pass
